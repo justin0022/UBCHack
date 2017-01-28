8f58b3a216c62fb60af3a08ae76b1df40bdd6dd9
=Shirley Yin <sshirley.yin@gmail.com>
2017-01-28 14:00:20 -0800

    parse course structure and create json object

--- parse.py
+++ parse.py
@@ -34,15 +34,70 @@ def parse_user_data():
 
 
 
-def generate_json_object():
-    pass
+def generate_json_object(data):
+    
+    final_dict = {}
+
+    for row in data:
+        if row["category"] == "course":
+            final_dict["course"] = row["name"]
+            final_dict["element_order"] = row["element_order"]
+            final_dict["children"] = []
+        elif row["category"] == "chapter":
+            final_dict["children"].append({"category": row["category"], "name": row["name"], "element_order": row["element_order"], "url_name": row["url_name"], "parent": row["parent"], "children": []})
+        elif row["category"] == "sequential":
+            chapter = [ch for ch in final_dict["children"] if ch["url_name"] == row["parent"]][0]
+
+            chapter["children"].append({"category": row["category"], "name": row["name"], "element_order": row["element_order"], "url_name": row["url_name"], "parent": chapter["url_name"], "children": []})
+
+        elif row["category"] == "vertical":
+            sequential_id = row["parent"]
+            all_chapters = [ch["children"] for ch in final_dict["children"]]
+
+            all_sequentials_flattened = []
+
+            for sequential_list in all_chapters:
+                for sequential in sequential_list:
+                    all_sequentials_flattened.append(sequential)
+            
+            sequential = [sq for sq in all_sequentials_flattened if sq["url_name"] == sequential_id][0]
+
+            sequential["children"].append({"category": "vertical", "name": row["name"], "element_order": row["element_order"], "url_name": row["url_name"], "parent": row["parent"], "children": []})
+
+        elif row["category"] in ["html", "video", "problem"]:
+
+            vertical_id = row["parent"]
+
+            all_chapters = [ch["children"] for ch in final_dict["children"]]
+
+            all_sequentials_flattened = []
+
+            for sequential_list in all_chapters:
+                for sequential in sequential_list:
+                    all_sequentials_flattened.append(sequential)
+            
+            all_verticals_flattened = []
+
+            for sequential in all_sequentials_flattened:
+                for vertical in sequential["children"]:
+                    all_verticals_flattened.append(vertical)
+
+            vertical = [ve for ve in all_verticals_flattened if ve["url_name"] == vertical_id][0]
+
+            vertical["children"].append({"category": row["category"], "name": row["name"], "element_order": row["element_order"], "url_name": row["url_name"], "parent": row["parent"]})
+
+    json_obj = json.dumps(final_dict)
+
+    fout = open("data.json", "w")
+    fout.write(json_obj)
+    print(json_obj)
 
 
 def main():
 
     course_structure = parse_course_structure()
 
-
+    generate_json_object(course_structure)
 
 
 
